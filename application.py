7c9cf5f4c78e4c89047e0093ab84975f6566f760
saik <bhargavinuthalapati@Bhargavis-MacBook-Pro.local>
2021-08-01 15:03:48 +0800

    added anaother message

--- application.py
+++ application.py
@@ -2,7 +2,7 @@ from flask import Flask
 application=Flask(__name__)
 @application.route('/')
 def hello_world():
-    return 'Hi Flask this is Sai'
+    return 'Hi Flask, this is Sai and finally I did it'
 
 if __name__=="__main__":
    application.run(debug=True)
